4927138c65db88387fb6a72d68dce2af098b2ae5
aditya191201 <deshpande.adu01@gmail.com>
2022-09-18 14:46:44 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. MAVS Frontend.
+          Edit <code>src/App.js</code> and save to reload. Aditya here. 
         </p>
         <a
           className="App-link"
